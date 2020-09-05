068f98e7a575d6cfef905e33be1307c154262de1
poncem91 <mafe.ponce@gmail.com>
2020-09-04 23:49:32 -0600

    add basic game initial setup

--- client.py
+++ client.py
@@ -1,6 +1,9 @@
 import socket
 import sys
 
+game_started = False
+play_count = 0
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 if len(sys.argv) < 3:
@@ -13,11 +16,34 @@ server_name = socket.gethostbyname(sys.argv[1])
 
 client_socket.connect((server_name, server_port))
 
-input_client = input("Say something ")
-while input_client.lower().strip() != 'bye':
-    client_socket.sendall(input_client.encode())
+
+def get_request(url):
+    return "GET " + url + " HTTP/1.1\r\nHost: " + server_name + "\r\nAccept: text/html\r\n\r\n"
+
+
+print("Welcome to a game of Rock, Paper, Scissors!")
+
+while True:
+    if not game_started:
+        client_socket.sendall(get_request("/start-game").encode("utf-8"))
+        http_response = client_socket.recv(1024)
+        response_status = http_response[1]
+        if response_status == "200":
+            game_started = True
+
+    input_client = input("Commands:\nR - Rock\nP - Paper\nS - Scissors\nRESET - Reset Game\nQ - Quit\n")
+
+    if input_client.upper() == "Q":
+        client_socket.sendall(get_request("/quit-game").encode("utf-8"))
+        break
+
+    elif input_client.upper() == "R":
+        pass
+
+    elif input_client.upper() == "S":
+        client_socket.sendall(input_client.encode("utf-8"))
     response = client_socket.recv(1024)
-    print('from server: ', response.decode())
+    print('from server: ', response.decode("utf-8"))
     input_client = input("Say something ")
 
 client_socket.close()
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 from _thread import *
+import time
 
 if len(sys.argv) < 2:
     sys.exit("Missing port number")
@@ -17,15 +18,38 @@ server_socket.listen(2)
 print("Server", server_name, "at port", server_port, "ready to receive.")
 
 
-def client_thread(connection):
+def client_thread(connection, player_address):
+    print("Player is connected with address", player_address)
     while True:
-        sentence = connection.recv(1024).decode()
-        capitalized = sentence.upper()
-        connection.sendall(capitalized.encode())
+        request = connection.recv(1024).decode("utf-8")
+        if not request:
+            break
+        request_list = request.split(' ')
+        request_method = request_list[0]
+        request_url = request_list[1]
+        print("Player", player_address, "sent a", request_method, "request to", request_url)
+        response = ""
+
+        if request_url == "/start-game":
+            with open("game_file.txt", "r") as game_file:
+                count = len(game_file.readlines())
+            with open("game_file.txt", "a+") as game_file:
+                count += 1
+                game_file.write(str(count) + "\n")
+                print("Player number", count, "has been saved")
+
+            if count == 1:
+                print("Waiting for second player...")
+
+            response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n"
+
+        if request_url == "/quit-game":
+            open("game_file.txt", "w").close()
+
+        connection.sendall(response.encode("utf-8"))
     connection.close()
 
 
 while True:
     connection_socket, address = server_socket.accept()
-    print("Server connected to", address)
-    start_new_thread(client_thread, (connection_socket, ))
+    start_new_thread(client_thread, (connection_socket, address))
