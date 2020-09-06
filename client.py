c0f0d8cddff93cac03f962b5de78ceb7dace09f6
poncem91 <mafe.ponce@gmail.com>
2020-09-05 18:44:14 -0600

    add rock, paper, scissors functionality

--- client.py
+++ client.py
@@ -1,51 +1,147 @@
 import socket
 import sys
 
-game_started = False
-play_count = 0
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def main():
+    game_started = False
+    play_thrown = False
+    play_count = 0
 
-if len(sys.argv) < 3:
-    sys.exit("Missing port number and/or address")
-try:
-    server_port = int(sys.argv[2])
-except ValueError:
-    sys.exit("Invalid port number")
-server_name = socket.gethostbyname(sys.argv[1])
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client_socket.connect((server_name, server_port))
+    if len(sys.argv) < 3:
+        sys.exit("Missing port number and/or address")
+    try:
+        server_port = int(sys.argv[2])
+    except ValueError:
+        sys.exit("Invalid port number")
+    server_name = socket.gethostbyname(sys.argv[1])
+
+    client_socket.connect((server_name, server_port))
+
+    print("Welcome to a game of Rock, Paper, Scissors!")
+    print("\nCommands:\nR - Rock\nP - Paper\nS - Scissors\nGS - Get overall game score\nPR - Get play "
+          "result\nRESET - Reset Game\nQ - Quit\n? - Show Commands\n")
+
+    while True:
+        http_response = ""
+
+        # setup game
+        if not game_started:
+            client_socket.sendall(get_request("/start-game", server_name).encode("utf-8"))
+            http_response = client_socket.recv(1024).decode("utf-8")
+            response_list = http_response.split()
+            response_status = response_list[1]
+            if response_status == "200":
+                game_started = True
+                player_id = response_list[5]
+                print("You are player #", player_id)
+            else:
+                print("There was an error starting the game")
+                break
+
+        input_client = input("\nCommand: ")
+
+        # quit application
+        if input_client.upper() == "Q":
+            client_socket.sendall(get_request("/quit-game", server_name).encode("utf-8"))
+            break
+
+        # request game score
+        elif input_client.upper() == "GS":
+            client_socket.sendall((get_request("/gamescore", server_name).encode("utf-8")))
+            http_response = client_socket.recv(1024).decode("utf-8")
+            response_list = http_response.split()
+            response_status = response_list[1]
+            if response_status != "200":
+                print("Error: Could not retrieve game score. Exiting game.")
+                break
+            try:
+                player_1_score = response_list[5]
+                player_2_score = response_list[6]
+            except IndexError:
+                print("Error: Could not retrieve game score. Exiting game.")
+                break
+            print("\nGame Score - WINS-LOSES-TIES\n")
+            if player_id == "1":
+                print("Your score:", player_1_score)
+                print("Opponent score:", player_2_score)
+            else:
+                print("Your score:", player_2_score)
+                print("Opponent score:", player_1_score)
+
+        # send Rock play
+        elif input_client.upper() == "R":
+            successful_play = send_play("R", server_name, player_id, client_socket)
+            if successful_play:
+                play_thrown = True
+                play_count += 1
+
+        # send Paper play
+        elif input_client.upper() == "P":
+            successful_play = send_play("P", server_name, player_id, client_socket)
+            if successful_play:
+                play_thrown = True
+                play_count += 1
+
+        # send Scissors play
+        elif input_client.upper() == "S":
+            successful_play = send_play("S", server_name, player_id, client_socket)
+            if successful_play:
+                play_thrown = True
+                play_count += 1
+
+        # gets play result
+        elif input_client.upper() == "PR":
+            if not play_thrown:
+                print("You haven't made a move yet. Please make a move first")
+            else:
+                client_socket.sendall(get_request("/play", server_name).encode("utf-8"))
+                print("\nPlease wait...\n")
+                http_response = client_socket.recv(1024).decode("utf-8")
+                response_list = http_response.split()
+                try:
+                    response_body = response_list[5]
+                except IndexError:
+                    print("Error: Could not retrieve play result. Exiting game.")
+                    break
+                results = response_body.split("-")
+                player_result = results[int(player_id) - 1]
+                if player_result == "W":
+                    print("You won!")
+                elif player_result == "L":
+                    print("You lost...")
+                elif player_result == "T":
+                    print("You tied!")
+
+    client_socket.close()
 
 
-def get_request(url):
-    return "GET " + url + " HTTP/1.1\r\nHost: " + server_name + "\r\nAccept: text/html\r\n\r\n"
+def get_request(url, server):
+    return "GET " + url + " HTTP/1.1\r\nHost: " + server + "\r\nAccept: text/html\r\n\r\n"
 
 
-print("Welcome to a game of Rock, Paper, Scissors!")
+def post_request(url, server, player, move):
+    return "POST " + url + " HTTP/1.1\r\nHost: " + server + "\r\nContent-Type: text/html\r\n\r\nplayer=" + player + "&move=" + move
 
-while True:
-    if not game_started:
-        client_socket.sendall(get_request("/start-game").encode("utf-8"))
-        http_response = client_socket.recv(1024).decode("utf-8")
-        response_list = http_response.split(" ")
-        response_status = response_list[1]
-        if response_status == "200":
-            game_started = True
 
-    input_client = input("Commands:\nR - Rock\nP - Paper\nS - Scissors\nGS - Get overall game score\nPS - Get current "
-                         "play score\nRESET - Reset Game\nQ - Quit\n")
+def send_play(play, server, player, c_socket):
+    c_socket.sendall(post_request("/play", server, player, play).encode("utf-8"))
+    http_response = c_socket.recv(1024).decode("utf-8")
+    response_list = http_response.split()
+    response_status = response_list[1]
+    if response_status == "200":
+        if play == "R":
+            print("\nYou threw rock!")
+        elif play == "S":
+            print("\nYou threw scissors!")
+        elif play == "P":
+            print("\nYou threw paper!")
+        return True
+    elif response_status == "409":
+        print("\nYou can't throw duplicate moves on the same play")
+        return False
 
-    if input_client.upper() == "Q":
-        client_socket.sendall(get_request("/quit-game").encode("utf-8"))
-        break
 
-    elif input_client.upper() == "GS":
-        client_socket.sendall((get_request("/gamescore").encode("utf-8")))
-
-    elif input_client.upper() == "S":
-        client_socket.sendall(input_client.encode("utf-8"))
-
-    http_response = client_socket.recv(1024).decode("utf-8")
-    print('from server:\n' + http_response)
-
-client_socket.close()
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,59 +1,191 @@
 import socket
 import sys
 from _thread import *
+import pickle
+import os
 import time
 
-if len(sys.argv) < 2:
-    sys.exit("Missing port number")
-try:
-    server_port = int(sys.argv[1])
-except ValueError:
-    sys.exit("Invalid port number")
 
-server_name = socket.gethostbyname(socket.gethostname())
+def main():
+    if len(sys.argv) < 2:
+        sys.exit("Missing port number")
+    try:
+        server_port = int(sys.argv[1])
+    except ValueError:
+        sys.exit("Invalid port number")
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((server_name, server_port))
-server_socket.listen(2)
-print("Server", server_name, "at port", server_port, "ready to receive.")
+    server_name = socket.gethostbyname(socket.gethostname())
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((server_name, server_port))
+    server_socket.listen(2)
+    print("Server", server_name, "at port", server_port, "ready to receive.")
+
+    # deletes game data file from previous game if it exists
+    if os.path.isfile("game_file.txt"):
+        os.remove("game_file.txt")
+
+    while True:
+        connection_socket, address = server_socket.accept()
+        start_new_thread(client_thread, (connection_socket, address))
 
 
 def client_thread(connection, player_address):
     print("Player is connected with address", player_address)
     while True:
-        request = connection.recv(1024).decode("utf-8")
-        if not request:
+        client_request = connection.recv(1024).decode("utf-8")
+        if not client_request:
             break
-        request_list = request.split(' ')
+
+        # parses HTTP request
+        request_list = client_request.split()
         request_method = request_list[0]
-        request_url = request_list[1]
-        print("Player", player_address, "sent a", request_method, "request to", request_url)
+        full_request_url = request_list[1]
+        request_url = full_request_url.split("?")[0]
+
+        # if parameters have been passed as a query string it stores them separately
+        if len(full_request_url.split("?")) > 1:
+            request_url_params = full_request_url.split("?")[1]
+
+        print(player_address, "sent a", request_method, "request to", request_url)
+
         response_ok_header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
 
         if request_url == "/start-game":
-            with open("game_file.txt", "r") as game_file:
-                count = len(game_file.readlines())
-            with open("game_file.txt", "a+") as game_file:
-                count += 1
-                game_file.write(str(count) + "\n")
-                print("Player number", count, "has been saved")
+            if not os.path.isfile("game_file.txt"):
+                game_data = {'player_count': 1,
+                             'player_1': {'W': 0, 'L': 0, 'T': 0},
+                             'player_2': {'W': 0, 'L': 0, 'T': 0},
+                             'current_play': [None, None],
+                             'current_play_result': None,
+                             'reset': [False, False]}
+                with open("game_file.txt", "wb") as game_file:
+                    pickle.dump(game_data, game_file)
+            else:
+                with open("game_file.txt", "rb") as game_file:
+                    game_data = pickle.load(game_file)
+                    if game_data["player_count"] >= 2:
+                        break
+                    game_data["player_count"] += 1
 
-            if count == 1:
+                with open("game_file.txt", "wb") as game_file:
+                    pickle.dump(game_data, game_file)
+            print("Player number", game_data["player_count"], "has been saved")
+
+            if game_data["player_count"] == 1:
                 print("Waiting for second player...")
 
-            response = response_ok_header
+            response = response_ok_header + str(game_data["player_count"])
 
-        if request_url == "/quit-game":
-            open("game_file.txt", "w").close()
+        elif request_url == "/quit-game":
+            if os.path.isfile("game_file.txt"):
+                os.remove("game_file.txt")
 
-        if request_url == "/gamescore":
-            with open("game_file.txt", "r") as game_file:
-                response = response_ok_header + game_file.read() + "\r\n\r\n"
+        elif request_url == "/gamescore":
+            with open("game_file.txt", "rb") as game_file:
+                game_data = pickle.load(game_file)
+                player_1 = get_player_score(1, game_data)
+                player_2 = get_player_score(2, game_data)
+                response = response_ok_header + player_1 + "\n" + player_2
+
+        elif request_url == "/play":
+
+            # POST request to /play path will set a move for the play
+            if request_method == "POST":
+                try:
+                    request_body = request_list[7]
+                except IndexError:
+                    print("Missing POST body")
+                    break
+                params = request_body.split("&")
+                player_num = int(params[0].split("=")[1])
+                move = params[1].split("=")[1]
+                with open("game_file.txt", "rb") as game_file:
+                    game_data = pickle.load(game_file)
+
+                # only allows the move to be updated if there isn't a move for that play yet
+                if game_data["current_play"][player_num - 1] is None:
+
+                    # sets current play's move
+                    game_data["current_play"][player_num - 1] = move
+
+                    # updates file
+                    with open("game_file.txt", "wb") as game_file:
+                        pickle.dump(game_data, game_file)
+
+                    print("Current play updated to... ", end='')
+                    print(game_data["current_play"])
+
+                    response = response_ok_header
+
+                    # if both plays are in it calculates the result and updates game file
+                    if game_data["current_play"][0] is not None and game_data["current_play"][1] is not None:
+                        player_1_move = game_data["current_play"][0]
+                        player_2_move = game_data["current_play"][1]
+
+                        if player_1_move == "R" and player_2_move == "P":
+                            result = "L-W"
+                        elif player_1_move == "R" and player_2_move == "S":
+                            result = "W-L"
+                        elif player_1_move == "P" and player_2_move == "R":
+                            result = "W-L"
+                        elif player_1_move == "P" and player_2_move == "S":
+                            result = "L-W"
+                        elif player_1_move == "S" and player_2_move == "R":
+                            result = "L-W"
+                        elif player_1_move == "S" and player_2_move == "P":
+                            result = "W-L"
+                        else:
+                            result = "T-T"
+
+                        # updates player's game score
+                        result_player_1 = result.split("-")[0]
+                        result_player_2 = result.split("-")[1]
+                        game_data["player_1"][result_player_1] += 1
+                        game_data["player_2"][result_player_2] += 1
+                        game_data["current_play_result"] = result
+                        with open("game_file.txt", "wb") as game_file:
+                            pickle.dump(game_data, game_file)
+
+                # otherwise it sends a 409 error code
+                else:
+                    response = "HTTP/1.1 409 Conflict\r\nContent-Type: text/html\r\n\r\n"
+
+            # GET request to /play path will return the play score
+            elif request_method == "GET":
+                with open("game_file.txt", "rb") as game_file:
+                    game_data = pickle.load(game_file)
+
+                if game_data["current_play_result"] is None:
+                    print("Waiting for both players to have made a move...")
+                    counter = 0
+                    while game_data["current_play_result"] is None and counter < 50:
+                        time.sleep(3)
+                        with open("game_file.txt", "rb") as game_file:
+                            game_data = pickle.load(game_file)
+                        counter += 1
+                    if counter >= 50:
+                        print("Error: Request Timeout while waiting for player to make a move")
+                        response = "HTTP/1.1 408 Request Timeout\r\nContent-Type: text/html\r\n\r\n"
+
+                if game_data["current_play_result"] is not None:
+                    result = game_data["current_play_result"]
+                    print("Requested play result is:", result)
+
+                    response = response_ok_header + result
+
+        # 404 error code sent if a request has been sent to an non-existing resource
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
         connection.sendall(response.encode("utf-8"))
     connection.close()
 
 
-while True:
-    connection_socket, address = server_socket.accept()
-    start_new_thread(client_thread, (connection_socket, address))
+def get_player_score(num, game_data):
+    player = "player_" + str(num)
+    return str(game_data[player]["W"]) + "-" + str(game_data[player]["L"]) + "-" + str(game_data[player]["T"])
+
+
+if __name__ == "__main__":
+    main()
